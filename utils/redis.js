a0198cbfa8ebb59756a53164d68f8cdc7920405d
Ahmad <ahmad.new.m.v@gmial.com>
2024-12-11 14:59:52 +0200

    1st task's solution

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+    constructor(host='localhost', port=6379) {
+        this.client = redis.createClient({host, port});
+        this.client.on('error', (error) => console.log(error));
+    }
+
+    async isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        this.client.get = promisify(this.client.get);
+        return this.client.get(key);
+    }
+
+    async set(key, value, duration) {
+        this.client.set = promisify(this.client.set);
+        return this.client.set(key, value, 'EX', duration);
+    }
+
+    async del(key) {
+        return this.client.del(key);
+    }
+}
+
+const redisClient = new RedisClient('localhost', 6379);
+
+module.exports = redisClient;
